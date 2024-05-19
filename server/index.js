f1c0dff9fbd57b5d8cf54cd9c6e3a091276918af
DEBANJAN MUKHERJEE <debanjanmukherjee015@gmail.com>
2024-05-19 17:58:47 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import { ecoActionRouter } from "./controller/EcoActionController.js";
 const app = express();
 app.use(
   cors({
-    origin: ["https://ecobase.vercel.app"],
+    origin: ["http://localhost:5173"],
     method: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );

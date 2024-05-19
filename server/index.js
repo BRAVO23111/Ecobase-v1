917f516ef62b13cefd0158bd9fa2b804ddb2614c
DEBANJAN MUKHERJEE <debanjanmukherjee015@gmail.com>
2024-05-19 18:28:24 +0530

    added frontend links

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://ecobase-v1.vercel.app"],
+    origin: ["https://ecobase-v1-spey.vercel.app"],
     method: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials : true
   })  
